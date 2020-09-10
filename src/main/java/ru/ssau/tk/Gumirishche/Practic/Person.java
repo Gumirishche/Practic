81b2adba86470f1aec7e5722602da5b0f19602b1
Gumirishche <gumer2001@mail.ru>
2020-09-10 21:02:59 +0400

    создал класс Person, приватные поля firstName, lastNAme, pasportId, а так же геттеры и сеттеры

---
+++ src/main/java/ru/ssau/tk/Gumirishche/Practic/Person.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.Gumirishche.Practic;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+    String getFirstName(){
+        return firstName=firstName;
+    }
+    String getLastName(){
+        return lastName=lastName;
+    }
+    int getPassportId(){
+        return passportId=passportId;
+    }
+    void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+    void setPassportId(int passportId){
+        this.passportId=passportId;
+    }
+}
