dcd6e7bcbaeab15b16d27a244090c2e9ffee0e02
Gumirishche <gumer2001@mail.ru>
2020-10-11 20:34:59 +0400

    2.2 "Ctrl+Alt+L"

--- src/main/java/ru/ssau/tk/Gumirishche/Practic/GeneratorMassif.java
+++ src/main/java/ru/ssau/tk/Gumirishche/Practic/GeneratorMassif.java
@@ -1,14 +1,13 @@
 package ru.ssau.tk.Gumirishche.Practic;
 
 public class GeneratorMassif {
-    public int[] generatorMassif(int size){
-        int[] massif=new int[size];
-        for(int i =0;i<size;i++){
-            if(i==0||i==size-1){
-                massif[i]=2;
-            }
-            else{
-                massif[i]=1;
+    public int[] generatorMassif(int size) {
+        int[] massif = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (i == 0 || i == size - 1) {
+                massif[i] = 2;
+            } else {
+                massif[i] = 1;
             }
         }
         return massif;
--- src/test/java/ru/ssau/tk/Gumirishche/Practic/GeneratorMassifTest.java
+++ src/test/java/ru/ssau/tk/Gumirishche/Practic/GeneratorMassifTest.java
@@ -5,12 +5,13 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class GeneratorMassifTest {
-    GeneratorMassif generatorMassif=new GeneratorMassif();
-    int[] massifTest1=new int[]{2,1,1,1,1,2};
+    GeneratorMassif generatorMassif = new GeneratorMassif();
+    int[] massifTest1 = new int[]{2, 1, 1, 1, 1, 2};
+
     @Test
-    public void testGeneratorMassif(){
-        assertEquals(generatorMassif.generatorMassif(6),massifTest1);
-        assertEquals(generatorMassif.generatorMassif(2),new int[]{2,2});
-        assertEquals(generatorMassif.generatorMassif(4),new int[]{2,1,1,2});
+    public void testGeneratorMassif() {
+        assertEquals(generatorMassif.generatorMassif(6), massifTest1);
+        assertEquals(generatorMassif.generatorMassif(2), new int[]{2, 2});
+        assertEquals(generatorMassif.generatorMassif(4), new int[]{2, 1, 1, 2});
     }
 }
