d6027244ed7e868166c4b5219ed33d37f5b85943
Gumirishche <gumer2001@mail.ru>
2020-10-19 20:02:46 +0400

    2.5 Создан метод fibonacciMass которы создает масиц размером n из чисел фибоначчи

---
+++ src/test/java/ru/ssau/tk/Gumirishche/Practic/Massif2_5Test.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.Gumirishche.Practic;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Massif2_5Test {
+    Massif2_5 massif2_5=new Massif2_5();
+    @Test
+    public void testMassif2_5(){
+        assertEquals(massif2_5.fibonacciMass(5),new int[]{1,1,2,3,5});
+        assertEquals(massif2_5.fibonacciMass(10),new int[]{1,1,2,3,5,8,13,21,34,55});
+        assertEquals(massif2_5.fibonacciMass(6),new int[]{1,1,2,3,5,8});
+    }
+
+}
