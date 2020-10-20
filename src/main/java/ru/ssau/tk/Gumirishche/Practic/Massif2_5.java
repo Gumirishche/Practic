9e253a912eb61862ad85155cc8d8a36e7c59c90b
Gumirishche <gumer2001@mail.ru>
2020-10-20 16:35:28 +0400

    2.6 Создан класс Massif2_6, метод которого создает массив состоящий из квадратов индекса, создан тест

--- src/main/java/ru/ssau/tk/Gumirishche/Practic/Massif2_5.java
+++ src/main/java/ru/ssau/tk/Gumirishche/Practic/Massif2_5.java
@@ -1,12 +1,12 @@
 package ru.ssau.tk.Gumirishche.Practic;
 
 public class Massif2_5 {
-    public int[] fibonacciMass(int size){
-        int[] fibonacci=new int[size];
-        fibonacci[0]=1;
-        fibonacci[1]=1;
-        for(int i=2;i<size;i++){
-            fibonacci[i]=fibonacci[i-1]+fibonacci[i-2];
+    public int[] fibonacciMass(int size) {
+        int[] fibonacci = new int[size];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        for (int i = 2; i < size; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
         return fibonacci;
     }
