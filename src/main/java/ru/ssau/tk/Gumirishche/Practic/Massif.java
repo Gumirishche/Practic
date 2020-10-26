7bdff7c6d2b108c41e387931f9cd51e06a80dcbf
Gumirishche <gumer2001@mail.ru>
2020-10-26 12:26:42 +0400

    2.11 Создан метод, возвращающий массив, содержащий все делители целого числа, являющегося входным аргументом метода. Покрыл метод тестами. Примечание: делитель числа не может быть больше, чем корень из этого числа.

--- src/main/java/ru/ssau/tk/Gumirishche/Practic/Massif.java
+++ src/main/java/ru/ssau/tk/Gumirishche/Practic/Massif.java
@@ -1,4 +1,5 @@
 package ru.ssau.tk.Gumirishche.Practic;
+
 import java.lang.Math;
 
 public class Massif {
@@ -15,29 +16,44 @@ public class Massif {
         }
         return massif;
     }
+
     public long[] geometricMassif(int size, int first, double division) {
         long[] massif = new long[size];
         double massifInteger;
         double massifRemains;
         massif[0] = first;
         for (int i = 1; i < size; i++) {
-            massifInteger = massif[i-1]/division;
-            massifRemains=massif[i-1]%division;
-            long result=Math.round(massifInteger+massifRemains);
-            massif[i]=result;
+            massifInteger = massif[i - 1] / division;
+            massifRemains = massif[i - 1] % division;
+            long result = Math.round(massifInteger + massifRemains);
+            massif[i] = result;
         }
         return massif;
     }
+
     public double[] geometricMassif(int size, double first, double division) {
         double[] massif = new double[size];
         double massifInteger;
         double massifRemains;
         massif[0] = first;
         for (int i = 1; i < size; i++) {
-            massifInteger = massif[i-1]/division;
-            massifRemains=massif[i-1]%division;
-            double result=massifInteger+massifRemains;
-            massif[i]=result;
+            massifInteger = massif[i - 1] / division;
+            massifRemains = massif[i - 1] % division;
+            double result = massifInteger + massifRemains;
+            massif[i] = result;
+        }
+        return massif;
+    }
+
+    public int[] divisionMassif(int number) {
+        int size = (int) ((number / (Math.sqrt(number)))-1);
+        int[] massif = new int[size];
+        int j = 0;
+        for (int i = 1; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                massif[j] = i;
+                j++;
+            }
         }
         return massif;
     }
@@ -47,23 +63,25 @@ public class Massif {
             System.out.println(x[i]);
         }
     }
-    public void paint(int size, int first, double division){
+
+    public void paint(int size, int first, double division) {
         long[] massif = new long[size];
         double massifInteger;
         double massifRemains;
         massif[0] = first;
         for (int i = 1; i < size; i++) {
-            massifInteger = massif[i-1]/division;
-            massifRemains=massif[i-1]%division;
-            long result=Math.round(massifInteger+massifRemains);
-            massif[i]=result;
+            massifInteger = massif[i - 1] / division;
+            massifRemains = massif[i - 1] % division;
+            long result = Math.round(massifInteger + massifRemains);
+            massif[i] = result;
             System.out.println(massif[i]);
         }
     }
 
     public static void main(String[] args) {
         Massif x = new Massif();
-        /*x.paint(x.arithmeticMassif(5, 1, 6));*/
-        x.paint(3,1,0.1);
+        /*x.paint(x.arithmeticMassif(5, 1, 6));
+        x.paint(3,1,0.1);*/
+        x.paint(x.divisionMassif(36));
     }
 }
--- src/test/java/ru/ssau/tk/Gumirishche/Practic/MassifTest.java
+++ src/test/java/ru/ssau/tk/Gumirishche/Practic/MassifTest.java
@@ -23,22 +23,31 @@ public class MassifTest {
         assertEquals(massif01.arithmeticMassif(1, 6, 13515), new int[]{6});
         assertEquals(massif01.arithmeticMassif(3, 20, -20), new int[]{20, 0, -20});
     }
+
     @Test
-    public void testGeometricMassifRound(){
-        assertEquals(massif01.geometricMassif(3,1,0.1),new long[]{1,10,100});
-        assertEquals(massif01.geometricMassif(1,100,462772),new long[]{100});
-        assertEquals(massif01.geometricMassif(3,100,5),new long[]{100,20,4});
+    public void testGeometricMassifRound() {
+        assertEquals(massif01.geometricMassif(3, 1, 0.1), new long[]{1, 10, 100});
+        assertEquals(massif01.geometricMassif(1, 100, 462772), new long[]{100});
+        assertEquals(massif01.geometricMassif(3, 100, 5), new long[]{100, 20, 4});
     }
+
     @Test
-    public void testGeometricMassif(){
-        double[]x1=massif01.geometricMassif(3,1d,0.1);
-        assertEquals(x1[0],1,0.001);
-        assertEquals(x1[1],10,0.1);
-        assertEquals(x1[2],100,2);
-        double[] x2=massif01.geometricMassif(3,100d,5);
-        assertEquals(x2[0],100,0.001);
-        assertEquals(x2[1],20,0.00001);
-        assertEquals(x2[2],4,0.0001);
+    public void testGeometricMassif() {
+        double[] x1 = massif01.geometricMassif(3, 1d, 0.1);
+        assertEquals(x1[0], 1, 0.001);
+        assertEquals(x1[1], 10, 0.1);
+        assertEquals(x1[2], 100, 2);
+        double[] x2 = massif01.geometricMassif(3, 100d, 5);
+        assertEquals(x2[0], 100, 0.001);
+        assertEquals(x2[1], 20, 0.00001);
+        assertEquals(x2[2], 4, 0.0001);
+    }
+
+    @Test
+    public void testDivisionMassif() {
+        assertEquals(massif01.divisionMassif(9), new int[]{1, 3});
+        assertEquals(massif01.divisionMassif(16), new int[]{1, 2, 4});
+        assertEquals(massif01.divisionMassif(36), new int[]{1, 2, 3, 4, 6});
     }
 
 }
