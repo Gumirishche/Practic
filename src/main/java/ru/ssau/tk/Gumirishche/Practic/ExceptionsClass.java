c9de6889aa495013bbc693c1c2b65773f55718bd
Gumirishche <gumer2001@mail.ru>
2020-12-29 19:59:36 +0400

    4.3 Создал метод, принимающий на вход массив строк и индексы m и n (целые числа). Метод должен вернуть символ, расположенный в строке с номером m на позиции с номером n. Покрыть метод тестами. Рассмотрел случаи, когда m не меньше, чем длина массива, и n не меньше, чем длина строки с номером m. Протестировал оба этих вида исключения.

--- src/main/java/ru/ssau/tk/Gumirishche/Practic/ExceptionsClass.java
+++ src/main/java/ru/ssau/tk/Gumirishche/Practic/ExceptionsClass.java
@@ -1,11 +1,15 @@
 package ru.ssau.tk.Gumirishche.Practic;
 
 public class ExceptionsClass {
-    public char[] StringMassif(String[] strings,int index){
-        char[] chars=new char[strings.length];
-        for(int i=0;i<strings.length;i++){
-            chars[i]=strings[i].charAt(index);
+    public char[] StringMassif(String[] strings, int index) {
+        char[] chars = new char[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            chars[i] = strings[i].charAt(index);
         }
         return chars;
+    }
+
+    public char StringsParts(String[] strings, int m, int n) {
+        return strings[m].charAt(n);
     }
 }
--- src/test/java/ru/ssau/tk/Gumirishche/Practic/ExceptionsClassTest.java
+++ src/test/java/ru/ssau/tk/Gumirishche/Practic/ExceptionsClassTest.java
@@ -5,15 +5,27 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class ExceptionsClassTest {
-    ExceptionsClass exceptionsClass=new ExceptionsClass();
+    ExceptionsClass exceptionsClass = new ExceptionsClass();
 
     @Test
-    public void testStringMassif(){
-        assertEquals(exceptionsClass.StringMassif(new String[]{"Hello","World"},2),new char[]{'l','r'});
-        assertEquals(exceptionsClass.StringMassif(new String[]{"яблоко","лёд","груша"},2),new char[]{'л','д','у'});
+    public void testStringMassif() {
+        assertEquals(exceptionsClass.StringMassif(new String[]{"Hello", "World"}, 2), new char[]{'l', 'r'});
+        assertEquals(exceptionsClass.StringMassif(new String[]{"яблоко", "лёд", "груша"}, 2), new char[]{'л', 'д', 'у'});
 
-        assertThrows(NullPointerException.class,()->{
-            exceptionsClass.StringMassif(new String[3],1);
+        assertThrows(NullPointerException.class, () -> {
+            exceptionsClass.StringMassif(new String[3], 1);
+        });
+    }
+
+    @Test
+    public void testStringsParts() {
+        assertEquals(exceptionsClass.StringsParts(new String[]{"Привет", "яблоко"}, 1, 2), 'л');
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            exceptionsClass.StringsParts(new String[]{"hey", "hay"}, 2, 1);
+        });
+        assertThrows(StringIndexOutOfBoundsException.class, () -> {
+            exceptionsClass.StringsParts(new String[]{"hey", "hay"}, 1, 3);
         });
     }
 }
